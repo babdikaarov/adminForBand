5f0dbca2ca3d0c796f00a44ba0a56aef8b289702
Beksultan A <babdikaarov@gmail.com>
2024-03-02 17:28:20 +0300

    feat: hide params on reset

--- src/Resources/ResetPassword/Reset.tsx
+++ src/Resources/ResetPassword/Reset.tsx
@@ -1,12 +1,23 @@
 import { Login } from "react-admin";
 import { ResetForm } from "./ResetForm";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 
 const Reset = () => {
     const [searchParams] = useSearchParams();
-    const token = searchParams.get("token")!;
- 
+    const [token, setToken] = useState('');
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        const param = searchParams.get("token")
+        if(!param) return
+        setToken(param);
+        const url = new URL(window.location.href);
+        url.searchParams.delete("token");
+        navigate("/auth-callback", { replace: true });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     return (
         <Login
             id="reset_background"
